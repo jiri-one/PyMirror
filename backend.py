bc67cf9ec3bb4cf218a5af1282cde2a5c2ed0608
jiri.one <nemec@jiri.one>
2021-08-28 12:49:16 +0200

    initial commit

---
+++ backend.py
@@ -0,0 +1,12 @@
+import numpy as np
+from random import randint
+
+# create empty play grid
+play_grid = MAP = np.zeros((10, 10), dtype=bool)
+
+insert_square_to_grid(empty_play_grid):
+    """This function isert randomly a square 2x2 to play grid."""
+    
+
+print(play_grid)
+    
